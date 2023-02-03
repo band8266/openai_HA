7f18f75da9cb39a348a60ebe2c31d6417f4b87a0
band8266 <band8266@gmail.com>
2023-02-03 11:35:59 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,42 +2,33 @@ import openai
 import logging
 import random
 from homeassistant.helpers import area_registry, template
-openai.api_key = API_KEY
+openai.api_key = "insert"
 _LOGGER = logging.getLogger(__name__)
 ATTR_PROMPT = "prompt"
 ATTR_TEMP = ""
-DEFAULT_NAME = "Please tell un unfunny joke"
-DEFAULT_PROMPT = """You are a very sarcastic smarthome assistant.  
-Overview of relevant info to the residence:\n
-  Residence is located at {{homelong}}, {{homelat}}\n
+DEFAULT_NAME = ""
+DEFAULT_PROMPT = """You are a smarthome assistant.\n
+Overview of relevant info to the home:\n
+  Home is located at {{homelong}}, {{homelat}}\n
 {% if is_state("sun.sun", "above_horizon") -%}
   The sun rose {{ relative_time(states.sun.sun.last_changed) }} ago.
 {%- else -%}
   The sun will rise at {{ as_timestamp(state_attr("sun.sun", "next_rising")) | timestamp_local }}.
 {%- endif %}
--Owners:
+-Owners:\n
 {% for person in people -%}
 {% if person.state == 'not_home' %}
-{{ person.name}} is at {{person.attributes.latitude}}, {{person.attributes.longitude}}
+{{ person.name}} is at {{person.attributes.latitude}}, {{person.attributes.longitude}}.
 {%- else -%}
-  {{ person.name}} is {{person.state}} 
+  {{ person.name}} is {{person.state}}. 
 {% endif -%}
 {% endfor %}
 {%- for skys in weather %}
-- The weather is {{skys.state}}\n
-{%- endfor %}
-{%- for cover in covers %}
-  {{ cover.name }} is {{cover.state}}\n
-{%- endfor %}
-{%- for light in lights %}
-  {{ light.name }} is {{light.state}}\n
-{%- endfor %}\n
-- Overview of less relevant info:
-{%- for random in randoms %}
-  {{ random.name }} is {{random.state}}\n
-{%- endfor %}\n
-Always respond with a joke or sarcasm.  You provide information about the residence, and suggest an action for the owners to do. You cannot ask questions or preform any tasks:\n
-Use the info to provide a helpful reponse to the follwoing event that was triggered and requires a verbal response from you:\n
+- The weather is {{skys.state}}.\n
+-Shades{%- endfor %}\n
+  {%- for cover in covers %}
+    {{ cover.name }} is {{cover.state}}.\n
+  {%- endfor %}
 """
 def setup(hass, config):
     """Set up is called when Home Assistant is loading our component."""
@@ -48,8 +39,8 @@ def setup(hass, config):
                 "weather": random.sample(hass.states.all('weather'), 1),
                 "people": hass.states.all('person'),
                 "lights": random.sample(hass.states.all('light'), 5),
-                "covers": random.sample(hass.states.all('cover'), 3),
-                "randoms": random.sample(hass.states.all(), 30),
+                "covers": random.sample(hass.states.all('cover'), 9),
+                "randoms": random.sample(hass.states.all(), 50),
                 "homelong": hass.config.longitude, 
                 "homelat": hass.config.latitude,
             }
@@ -57,21 +48,23 @@ def setup(hass, config):
     def generate_text(call):
         prompt = generate_prompt()
         prompt += call.data.get(ATTR_PROMPT, DEFAULT_NAME)
+        prompt += "Response:"
         temp = call.data.get(ATTR_TEMP, 0.5)
         completions = openai.Completion.create(
             engine="text-davinci-003",
             prompt=prompt,
-            max_tokens=2048,
+            max_tokens=50,
             n=1,
             stop=None,
             temperature=temp,
         )
 
+            
         text = completions.choices[0].text
-        hass.states.set("text.openai", text)
+        hass.states.set("text.openai", text, {"max": 255})
         _LOGGER.warning("openai", prompt)
 
     hass.services.register("openai", "openai", generate_text)
 
+    # Return boolean to indicate that initialization was successful.
     return True
-
