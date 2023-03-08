e2e5270b865a71da414297001e10ca717cb7f2f3
band8266 <band8266@gmail.com>
2023-03-08 15:41:04 -0700

    Update __init__.py
    change for gpt-3.5-turbo

--- __init__.py
+++ __init__.py
@@ -2,65 +2,29 @@ import openai
 import logging
 import random
 from homeassistant.helpers import area_registry, template
-openai.api_key = "insert"
+openai.api_key = "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"
 _LOGGER = logging.getLogger(__name__)
 ATTR_PROMPT = "prompt"
 ATTR_TEMP = ""
 DEFAULT_NAME = ""
-DEFAULT_PROMPT = """You are a smarthome assistant.\n
-Overview of relevant info to the home:\n
-  Home is located at {{homelong}}, {{homelat}}\n
-{% if is_state("sun.sun", "above_horizon") -%}
-  The sun rose {{ relative_time(states.sun.sun.last_changed) }} ago.
-{%- else -%}
-  The sun will rise at {{ as_timestamp(state_attr("sun.sun", "next_rising")) | timestamp_local }}.
-{%- endif %}
--Owners:\n
-{% for person in people -%}
-{% if person.state == 'not_home' %}
-{{ person.name}} is at {{person.attributes.latitude}}, {{person.attributes.longitude}}.
-{%- else -%}
-  {{ person.name}} is {{person.state}}. 
-{% endif -%}
-{% endfor %}
-{%- for skys in weather %}
-- The weather is {{skys.state}}.\n
--Shades{%- endfor %}\n
-  {%- for cover in covers %}
-    {{ cover.name }} is {{cover.state}}.\n
-  {%- endfor %}
-"""
 def setup(hass, config):
     """Set up is called when Home Assistant is loading our component."""
-    def generate_prompt() -> str:
-        """Generate a prompt for the user."""
-        return template.Template(DEFAULT_PROMPT, hass).async_render(
-            {
-                "weather": random.sample(hass.states.all('weather'), 1),
-                "people": hass.states.all('person'),
-                "lights": random.sample(hass.states.all('light'), 5),
-                "covers": random.sample(hass.states.all('cover'), 9),
-                "randoms": random.sample(hass.states.all(), 50),
-                "homelong": hass.config.longitude, 
-                "homelat": hass.config.latitude,
-            }
-        )
     def generate_text(call):
-        prompt = generate_prompt()
-        prompt += call.data.get(ATTR_PROMPT, DEFAULT_NAME)
-        prompt += "Response:"
-        temp = call.data.get(ATTR_TEMP, 0.5)
-        completions = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=prompt,
+        DEFAULT_PROMPT = [
+        {"role": "system", "content": "You are a smart home assistant.  The user provides some smart home data and a request. You respond with sarcasm and must be less than 255 characters."}
+]
+        prompt = DEFAULT_PROMPT
+        prompt.append( {"role": "user", "content": call.data.get(ATTR_PROMPT, DEFAULT_NAME) })
+        temp = call.data.get(ATTR_TEMP, 1)
+        completions = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=prompt,
             max_tokens=50,
             n=1,
             stop=None,
             temperature=temp,
         )
-
-            
-        text = completions.choices[0].text
+        text = completions.choices[0].message['content']
         hass.states.set("text.openai", text, {"max": 255})
         _LOGGER.warning("openai", prompt)
 
